a9d6b34d49b380a3a792d90d4df67d735dca508b
XXXXXiang <seanlin84119@Gmail.com>
2021-10-13 19:21:09 +0800

    add binary-decimal convert

---
+++ binary_decimal_convert/binary_decimal_convert.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void showBinary(unsigned int);
+unsigned int BinaryToDecimial(unsigned __int32);
+
+struct node {
+    int data;
+    struct node* next;
+};
+typedef struct node NODE;
+
+//int main() {
+//  showBinary(1234);
+//  BinaryToDecimial(0b10011010010);
+//  return 0;
+//}
+//
+void showBinary(unsigned int num) {
+    int digit = 0;
+    NODE* pre = NULL;
+    NODE* cur = NULL;
+    bool first = true;
+    while (num) {
+        cur = (NODE*)malloc(sizeof(NODE));
+        digit = num % 2;
+        num = num / 2;
+        cur->data = digit;//creat the linked list by "steping back"
+        cur->next = pre;
+        pre = cur;
+    }
+    printf("\n");
+
+    unsigned int sum = 0;
+    for (NODE* ptr=cur;ptr!=NULL;ptr=ptr->next) {
+        printf("%d",ptr->data);
+        //sum = (sum << 1) | (ptr->data); //Build a binary number.
+    }
+    printf("\n");
+    //printf("%d", sum);
+}
+
+unsigned int BinaryToDecimial(unsigned __int32 biNum) {
+    unsigned int sum = 0;
+    int bit = 0;
+    for (int i = 0;i<32;i++) {
+        bit = biNum & 1;
+        sum += bit * pow(2, i);
+        biNum = biNum >> 1;
+    }
+    printf("%d\n",sum);
+    return sum;
+}
